18e076e9c140ef98ee97bf7fcccd6cb79830c446
tusar-molla <tusarmolla96@gmail.com>
2025-03-06 12:58:38 +0600

    user insert update ok need to add getalluser

--- Interface/IUserService.cs
+++ Interface/IUserService.cs
@@ -1,6 +1,12 @@
-﻿namespace AccuStock.Interface
+﻿using AccuStock.Models;
+
+namespace AccuStock.Interface
 {
     public interface IUserService 
     {
+        
+        Task<bool> CreateUser(User user);
+        Task<bool> UpdateUser(User user);
+
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,7 +1,81 @@
-﻿using AccuStock.Interface;
+﻿using AccuStock.Data;
+using AccuStock.Interface;
+using AccuStock.Models;
+using Microsoft.EntityFrameworkCore;
 namespace AccuStock.Services
 {
     public class UserService: IUserService
     {
+        private readonly AppDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(AppDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<bool> CreateUser(User user)
+        {
+            try {
+                var subscriptionIdClaim = _httpContextAccessor.HttpContext?.User.FindFirst("SubscriptionId")?.Value;
+                user.SubscriptionId = int.Parse(subscriptionIdClaim!);
+                user.Password = GenerateResetPassword();
+                user.Status = UserStatusUpdate(false);
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+        public async Task<bool> UpdateUser(User user)
+        {
+            try
+            {
+                var subscriptionIdClaim = _httpContextAccessor.HttpContext?.User.FindFirst("SubscriptionId")?.Value;
+                if (subscriptionIdClaim == null)
+                {
+                    return false;
+                }
+                var existingUser = await _context.Users
+                    .FirstOrDefaultAsync(u => u.SubscriptionId == int.Parse(subscriptionIdClaim) && u.Id == user.Id);
+
+                if (existingUser == null)
+                {
+                    return false;
+                }
+
+                existingUser.FullName = user.FullName;
+                existingUser.Email = user.Email;
+                existingUser.Mobile = user.Mobile;
+                existingUser.Address = user.Address;
+                existingUser.RoleId = user.RoleId;
+                existingUser.BranchId = user.BranchId;
+
+                _context.Users.Update(existingUser);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public string GenerateResetPassword()
+        {
+            return "1234";
+        }
+
+        public bool UserStatusUpdate(bool status)
+        {
+            if(status)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
