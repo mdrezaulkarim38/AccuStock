073c950cb793cb601153e18ace7c584c12a0e8c6
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-03-10 16:08:00 +0600

    Add View Model Need to Remove DTo

---
+++ Models/ViewModels/Auth/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+namespace AccuStock.Models.ViewModels.Auth;
+public class RegisterViewModel
+{
+    [Required]
+    public string? FullName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; }
+    [Required]
+    public string? Password { get; set; }
+    [Required]
+    public int? ContactNumber { get; set; }
+    public int? RoleId { get; set; } = 1;
+}
