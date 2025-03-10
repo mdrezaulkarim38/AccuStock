f59c4d914e5834caaef07cf044f48408a408bedc
tusar-molla <tusarmolla96@gmail.com>
2025-03-10 16:00:26 +0600

    bankAccounts Controller and view need to update

---
+++ Controllers/BankAccountsController.cs
@@ -0,0 +1,26 @@
+﻿using AccuStock.Interface;
+using AccuStock.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccuStock.Controllers
+{
+    public class BankAccountsController : Controller
+    {       
+        private readonly IBankAccounts _bankAccounts;
+        private readonly IBranchService _branchService;
+
+        public BankAccountsController(IBankAccounts bankAccounts, IBranchService branchService)
+        {
+             _bankAccounts = bankAccounts;
+            _branchService = branchService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> BankAccountsList()
+        {
+            var branchList = await _branchService.GetAllBranches();
+            ViewBag.Branches = branchList;
+            var BankAccountsList = await _bankAccounts.GetAllBannk();
+            return View(BankAccountsList);
+        }
+    }
+}
