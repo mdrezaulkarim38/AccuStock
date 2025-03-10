d616b34e826eb5b57d2c5b1ee1a7e362a137caa6
tusar-molla <tusarmolla96@gmail.com>
2025-03-10 14:08:13 +0600

    create and update action add in controller for businessYEar

--- Controllers/BusinessYearController.cs
+++ Controllers/BusinessYearController.cs
@@ -1,4 +1,5 @@
 using AccuStock.Interface;
+using AccuStock.Models;
 using AccuStock.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -18,6 +19,33 @@ public class BusinessYearController : Controller
     {
         var businessYear = await _businessYearService.GetAllBusinessYear();
         return View(businessYear);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrUpdateBusinessYear(BusinessYear businessyear)
+    {
+        if (businessyear.Id == 0)
+        {
+            bool isCreated = await _businessYearService.CreateBusinessYear(businessyear);
+            if (!isCreated)
+            {
+                TempData["ErrorMessage"] = "This Business Year is already exists for this SubscriptionId.";
+                return RedirectToAction("BusinessYearList");
+            }
+            TempData["SuccessMessage"] = "Business Year Created Successfully";
+        }
+        else
+        {
+            bool isUpdated = await _businessYearService.UpdateBusinessYear(businessyear);
+            if (!isUpdated)
+            {
+                TempData["ErrorMessage"] = "Business Year already exists or update failed";
+                return RedirectToAction("BusinessYearList");
+            }
+            TempData["SuccessMessage"] = "BusinessYear Updated Successfully";
+        }
+
+        return RedirectToAction("BusinessYearList");
     }
 
     [HttpPost]
--- Interface/IBusinessYear.cs
+++ Interface/IBusinessYear.cs
@@ -4,6 +4,7 @@ namespace AccuStock.Interface;
 public interface IBusinessYear
 {
     Task<bool> CreateBusinessYear(BusinessYear businessYear);
+    Task<bool> UpdateBusinessYear(BusinessYear businessYear);
     Task<List<BusinessYear>> GetAllBusinessYear();
     Task<bool> ToggleBusinessYearStatusAsync(int busineesyearId);
 }
