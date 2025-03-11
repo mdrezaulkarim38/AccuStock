10b226ab642879a72fdfdda76a6118565ab2827b
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-03-11 14:43:49 +0600

    Now Add Type

---
+++ Migrations/20250311084252_TableChartGP.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccuStock.Migrations
+{
+    /// <inheritdoc />
+    public partial class TableChartGP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GroupID",
+                table: "ChartOfAccountTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                table: "ChartOfAccountTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupID",
+                table: "ChartOfAccountTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "ChartOfAccountTypes");
+        }
+    }
+}
--- Models/ChartOfAccountType.cs
+++ Models/ChartOfAccountType.cs
@@ -3,4 +3,6 @@ public class ChartOfAccountType
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+    public int ParentId { get; set; } = 0;
+    public int GroupID { get; set; } = 0;
 }
