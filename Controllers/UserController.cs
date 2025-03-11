4c50ceeadb788ece0b10009154e3725d5eb74f93
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-03-11 12:34:02 +0600

    fix some problem

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,70 +3,63 @@ using AccuStock.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AccuStock.Controllers
+namespace AccuStock.Controllers;
+
+[Authorize]
+public class UserController : Controller
 {
-    [Authorize]
-    public class UserController : Controller
+    private readonly ILogger<UserController> _logger;
+    private readonly IUserService _userService;
+    private readonly IBranchService _branchService;
+    public UserController(ILogger<UserController> logger, IUserService userService, IBranchService branchService)
     {
-        private readonly ILogger<UserController> _logger;
-        private readonly IUserService _userService;
-        private readonly IBranchService _branchService;
-        public UserController(ILogger<UserController> logger, IUserService userService, IBranchService branchService)
-        {
-            _logger = logger;
-            _userService = userService;
-            _branchService = branchService;
-        }
-        [HttpGet]
-        public async Task<IActionResult> UserList()
-        {
-            var branchList = await _branchService.GetAllBranches();
-            ViewBag.Branches = branchList;
-            var userList = await _userService.GetAllUsers();
-            return View(userList);
-        }
+        _logger = logger;
+        _userService = userService;
+        _branchService = branchService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> UserList()
+    {
+        var branchList = await _branchService.GetAllBranches();
+        ViewBag.Branches = branchList;
+        var userList = await _userService.GetAllUsers();
+        return View(userList);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateOrUpdateUser(User user)
+    [HttpPost]
+    public async Task<IActionResult> CreateOrUpdateUser(User user)
+    {
+        if (user.Id == 0)
         {
-            if (user.Id == 0)
+            bool isCreated = await _userService.CreateUser(user);
+            if (!isCreated)
             {
-                bool isCreated = await _userService.CreateUser(user);
-                if (!isCreated)
-                {
-                    TempData["ErrorMessage"] = "A User already exists for this SubscriptionId.";
-                    return RedirectToAction("Branch");
-                }
-                TempData["SuccessMessage"] = "User Created Successfully";
+                TempData["ErrorMessage"] = "A User already exists for this SubscriptionId.";
+                return RedirectToAction("UserList");
             }
-            else
+            TempData["SuccessMessage"] = "User Created Successfully";
+        }
+        else
+        {
+            bool isUpdated = await _userService.UpdateUser(user);
+            if (!isUpdated)
             {
-                bool isUpdated = await _userService.UpdateUser(user);
-                if (!isUpdated)
-                {
-                    TempData["ErrorMessage"] = "User name already exists or update failed";
-                    return RedirectToAction("Branch");
-                }
-                TempData["SuccessMessage"] = "User Updated Successfully";
+                TempData["ErrorMessage"] = "User name already exists or update failed";
+                return RedirectToAction("UserList");
             }
-            return RedirectToAction("UserList");
+            TempData["SuccessMessage"] = "User Updated Successfully";
         }
+        return RedirectToAction("UserList");
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> ToggleUserStatus(int userId)
-        {
-            bool success = await _userService.ToggleUserStatusAsync(userId);
+    [HttpPost]
+    public async Task<IActionResult> ToggleUserStatus(int userId)
+    {
+        bool success = await _userService.ToggleUserStatusAsync(userId);
 
-            if (!success)
-                return NotFound();
+        if (!success)
+            return NotFound();
 
-            return RedirectToAction("UserList");
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
+        return RedirectToAction("UserList");
     }
 }
