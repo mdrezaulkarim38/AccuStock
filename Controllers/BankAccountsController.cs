28a65f0c25fe0879f53937db4fef5392561502dd
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-03-11 11:59:33 +0600

    Add the Branch In here

--- Controllers/BankAccountsController.cs
+++ Controllers/BankAccountsController.cs
@@ -6,15 +6,19 @@ namespace AccuStock.Controllers;
 public class BankAccountsController : Controller
 {
     private readonly IBankAccountService _bankAccounts;
+    private readonly IBranchService _branch;
 
-    public BankAccountsController(IBankAccountService bankAccounts)
+    public BankAccountsController(IBankAccountService bankAccounts, IBranchService branch)
     {
         _bankAccounts = bankAccounts;
+        _branch = branch;
     }
 
     [HttpGet]
     public async Task<IActionResult> BankAccountsList()
     {
+        var branchList = await _branch.GetAllBranches();
+        ViewBag.Branches = branchList;
         var bankAccountsList = await _bankAccounts.GetAllBankAccount();
         return View(bankAccountsList);
     }
