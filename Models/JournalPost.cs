1b25e24330d41e209c7dc738695eb9a87b38c896
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-03-18 12:35:59 +0600

    two model is done

---
+++ Models/JournalPost.cs
@@ -0,0 +1,28 @@
+﻿namespace AccuStock.Models;
+
+public class JournalPost
+{
+    public int Id { get; set; }
+    public int BusinessYearId { get; set; }
+    public BusinessYear? BusinessYear { get; set; }
+    public int BranchId { get; set; }
+    public Branch? Branch { get; set; }
+    public string? VchNo { get; set; }
+    public DateTime? VchDate { get; set; }
+    public int? VchType {  get; set; }
+    public Decimal? Debit {  get; set; }
+    public Decimal? Credit {  get; set; }
+    public bool? Status { get; set; }
+    public int? PurchaseId { get; set; }
+    public int? VendorPaymentId { get; set; }
+    public int? SaleId {  get; set; }
+    public int? CustomerPaymentId {  get; set; }
+    public int? UserId {  get; set; }
+    public User? User { get; set; }
+    public string? RefNo { get; set; }
+    public string? Notes { get; set; }
+    public DateTime? Created { get; set; } = DateTime.Now;
+    public DateTime? Updated { get; set; }
+    public int SubscriptionId { get; set; }
+    public Subscription? Subscription { get; set; }
+}
---
+++ Models/JournalPostDetail.cs
@@ -0,0 +1,30 @@
+﻿namespace AccuStock.Models;
+
+public class JournalPostDetail
+{
+    public int Id { get; set; }
+    public int BusinessYearId { get; set; }
+    public BusinessYear? BusinessYear { get; set; }
+    public int BranchId { get; set; }
+    public Branch? Branch { get; set; }
+    public int JournalPostId {  get; set; }
+    public JournalPost? JournalPost { get; set; }
+    public int ChartOfAccountId {  get; set; }
+    public ChartOfAccount? ChartOfAccount { get; set; }
+    public string? VchNo { get; set; }
+    public DateTime? VchDate { get; set; }
+    public int? VchType { get; set; }
+    public Decimal? Debit { get; set; }
+    public Decimal? Credit { get; set; }
+    public string? ChqNo {  get; set; }
+    public DateTime? ChqDate {  get; set; }
+    public string? Remarks {  get; set; }
+    public bool Status {  get; set; }
+    public int? PurchaseId { get; set; }
+    public int? VendorPaymentId {  get; set; }
+    public int? SaleId {  get; set; }
+    public int? CustomerPaymentId {  get; set; }
+    public string? Description {  get; set; }
+    public int SubscriptionId { get; set; }
+    public Subscription? Subscription { get; set; }
+}
