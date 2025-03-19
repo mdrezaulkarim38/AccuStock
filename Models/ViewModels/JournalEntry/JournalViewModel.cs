37d844bf7e4352d0ffe21c0c676b9b9f1d4f4f49
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-03-19 11:36:21 +0600

    New View Created

--- Models/ViewModels/JournalEntry/JournalViewModel.cs
+++ Models/ViewModels/JournalEntry/JournalViewModel.cs
@@ -1,6 +1,9 @@
-﻿namespace AccuStock.Models.ViewModels.JournalEntry
+﻿namespace AccuStock.Models.ViewModels.JournalEntry;
+
+public class JournalViewModel
 {
-    public class JournalViewModel
-    {
-    }
+    public ChartOfAccount? chartOfAccount{ get; set; }
+    public ChartOfAccountType? chartOfAccountType{ get; set; }
+    public JournalPost? journalPost{ get; set; }
+    public JournalPostDetail? journalPostDetail{ get; set; }
 }
