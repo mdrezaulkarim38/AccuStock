18c67245d533d5692981f2f542ff580a5615d29a
tusar-molla <tusarmolla96@gmail.com>
2025-04-06 13:45:48 +0600

    GlInterfce created

---
+++ Interface/IGLedger.cs
@@ -0,0 +1,9 @@
+﻿using AccuStock.Models.ViewModels.GeneralLedger;
+
+namespace AccuStock.Interface
+{
+    public interface IGLedger
+    {
+        public Task<List<GLedger>> GetAllGLedger();
+    }
+}
---
+++ Models/ViewModels/GeneralLedger/GLedger.cs
@@ -0,0 +1,9 @@
+﻿namespace AccuStock.Models.ViewModels.GeneralLedger
+{
+    public class GLedger
+    {
+        public ChartOfAccount ChartOfAccount { get; set; }
+        public JournalPost JournalPost { get; set; }
+        public JournalPostDetail JournalPostDetail { get; set; }
+    }
+}
