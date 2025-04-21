a8fce5625be1d1d449f3aff9e37fee4819977129
tusar-molla <tusarmolla96@gmail.com>
2025-04-21 11:44:27 +0600

    Cat Controller done

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using AccuStock.Interface;
+using AccuStock.Models;
+using AccuStock.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccuStock.Controllers
+{
+    public class CategoryController : Controller
+    {
+       private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Category()
+        {
+            var categories = await _categoryService.GetAllCategory();
+            return View(categories);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrUpdateCat(Category category)
+        {
+            if(category.Id == 0)
+            {
+                bool isCreated = await _categoryService.CreateCategory(category);
+                if (!isCreated)
+                {
+                    TempData["ErrorMessage"] = "A Category already exists for this SubscriptionId.";
+                    return RedirectToAction("Category");
+                }
+                TempData["SuccessMessage"] = "Category Created Successfully";
+            }
+            else
+            {
+                bool isUpdated = await _categoryService.UpdateCategory(category);
+                if (!isUpdated)
+                {
+                    TempData["ErrorMessage"] = "Category name already exists or update failed";
+                    return RedirectToAction("Category");
+                }
+                TempData["SuccessMessage"] = "Category Updated Successfully";
+            }
+            return RedirectToAction("Category");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteCat(int id)
+        {
+            var result = await _categoryService.DeleteCategory(id);
+            TempData["SuccessMessage"] = "Category deleted successfully.";
+            return RedirectToAction("Category");
+        }
+
+    }
+}
