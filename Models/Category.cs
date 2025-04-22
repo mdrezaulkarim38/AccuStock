b1c71aeae29bfa84906d9c421748f4b5eaaab965
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-04-22 11:05:01 +0600

    Update model

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AccuStock.Models
 {
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int? ParentCategoryId { get; set; }
         [ForeignKey("ParentCategoryId")]
         public Category? ParentCategory { get; set; }
