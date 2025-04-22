84c98655b9d2fbbc493b7197d38c53387721a6fc
tusar-molla <tusarmolla96@gmail.com>
2025-04-22 16:29:10 +0600

    productModel add

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AccuStock.Models
+{
+    public class Product
+    {
+        public int Id {get; set;}
+        public string? Name { get; set; }
+        public string? Description { get; set;}
+        public string? Code { get; set; }
+        public int? BrandId { get; set; }
+        public Brand? Brand { get; set; }
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public string? ProductUnit { get; set; }
+        public string? ImagePath { get; set; }
+        [NotMapped]
+        public IFormFile? ProductImage { get; set; }
+        public int SubscriptionId { get; set; }
+        public Subscription? Subscription { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
