33ddba11045dcd9badf0e0ea64f215e2bdcc96f5
tusar-molla <tusarmolla96@gmail.com>
2025-04-23 14:18:50 +0600

    add controller product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,11 +1,74 @@
+using AccuStock.Interface;
+using AccuStock.Models;
+using AccuStock.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccuStock.Controllers;
 
 public class ProductController : Controller
 {
-    public ActionResult Index()
+    private readonly IProductService _productService;
+    public ProductController(IProductService productService)
     {
-        return View();
+        _productService = productService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Product()
+    {
+        var products = await _productService.GetAllProduct();
+        return View(products);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AddOrEditProduct(int id = 0)
+    {       
+        if(id == 0)
+        {
+            return View(new Product());
+        }
+        else
+        {
+            return View(await _productService.GetProductbyId(id));
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddOrEditProduct(Product product)
+    {
+        if(product.Id == 0)
+        {
+            bool isCreated = await _productService.CreateProduct(product);
+            if (!isCreated)
+            {
+                TempData["ErrorMessage"] = "A Product already exists for this SubscriptionId.";
+                return RedirectToAction("Product");
+            }
+            TempData["SuccessMessage"] = "Product Created Successfully";
+        }
+        else
+        {
+            bool isUpdated = await _productService.UpdateProduct(product);
+            if (!isUpdated)
+            {
+                TempData["ErrorMessage"] = "Product name already exists or update failed";
+                return RedirectToAction("Product");
+            }
+            TempData["SuccessMessage"] = "Product Updated Successfully";
+        }
+        return RedirectToAction("Product");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ToggleStatus(int productId)
+    {
+        bool success = await _productService.ToggleStatus(productId);
+
+        if (!success)
+        {
+            TempData["ErrorMessage"] = "Product Not Found";
+            return RedirectToAction("Product");
+        }
+
+        return RedirectToAction("Product");
     }
 }
