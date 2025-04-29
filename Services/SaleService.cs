54aecd4892f7a857bde57bf23ea927b5dbac39bb
tusar-molla <tusarmolla96@gmail.com>
2025-04-29 15:52:46 +0600

    Three service are created

--- Services/SaleService.cs
+++ Services/SaleService.cs
@@ -1,5 +1,6 @@
 ﻿using AccuStock.Data;
 using AccuStock.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AccuStock.Services
 {
@@ -12,15 +13,46 @@ namespace AccuStock.Services
             _context = context;
             _baseService = baseService;
         }
+        public async Task<List<Sale>> GetAllSale()
+        {
+            var subscriptionId = _baseService.GetSubscriptionId();
+            var userId = _baseService.GetUserId();
+            var branchId = await _baseService.GetBranchId(subscriptionId, userId);
 
-        //public async Task<List<Sale>> GetAllSale()
-        //{
-        //    var subscriptionIdClaim = _baseService.GetSubscriptionId();
-        //    return await _context.Sales
-        //        .Where(c => c.SubscriptionId == subscriptionIdClaim)
-        //        .Include(c => c.Customer)
-        //        .Include(c => c.Branch)
-        //        .ToListAsync();
-        //}
+            return await _context.Sales
+                .Where(c => c.SubscriptionId == subscriptionId && c.BranchId == branchId)
+                .Include(c => c.Customer)
+                .Include(c => c.Branch)
+                .ToListAsync();
+        }
+
+        public async Task<Sale?> GetSaleById(int saleId)
+        {
+            var subscriptionId = _baseService.GetSubscriptionId();
+            var userId = _baseService.GetUserId();
+            var branchId = await _baseService.GetBranchId(subscriptionId, userId);
+
+            if (branchId == 0)
+                return null; 
+            return await _context.Sales
+                .Where(s => s.Id == saleId && s.SubscriptionId == subscriptionId && s.BranchId == branchId)
+                .Include(s => s.Customer)
+                .Include(s => s.Branch)
+                .FirstOrDefaultAsync();
+        }
+        public async Task<int> GetSalebyInvNum(string invoiceNumber)
+        {
+            var subscriptionId = _baseService.GetSubscriptionId();
+            var userId = _baseService.GetUserId();
+            var branchId = await _baseService.GetBranchId(subscriptionId, userId);
+
+            var sale = await _context.Sales
+                .Where(s => s.InvoiceNumber == invoiceNumber
+                            && s.SubscriptionId == subscriptionId
+                            && s.BranchId == branchId)
+                .FirstOrDefaultAsync();
+            return sale?.Id ?? 0;
+        }
+
     }
 }
