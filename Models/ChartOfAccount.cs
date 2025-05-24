3e36cd0b62e98d3040894c044e8fcbc1ff3cbede
tusar-molla <tusarmolla96@gmail.com>
2025-05-24 10:45:51 +0600

    Datetime change

--- Models/ChartOfAccount.cs
+++ Models/ChartOfAccount.cs
@@ -16,5 +16,5 @@ public class ChartOfAccount
     public int UserId { get; set; }
     public User? User{ get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? UpdatedAt { get; set;}
+    public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
 }
